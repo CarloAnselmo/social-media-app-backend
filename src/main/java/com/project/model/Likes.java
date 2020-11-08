bf827decf45e96d2d7015f0167fdb5444f2275e0
CarloAnselmo <CarloAnselmo@gmail.com>
2020-11-07 19:10:08 -0500

    spring orm is working

--- src/main/java/com/project/model/Likes.java
+++ src/main/java/com/project/model/Likes.java
@@ -1,65 +1,65 @@
-package com.project.model;
-
-import javax.persistence.EmbeddedId;
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.MapsId;
-import javax.persistence.Table;
-
-@Entity
-@Table(name="LikeY")
-public class Likes {
-	
-	@EmbeddedId
-	LikesKey id;
-	
-	@ManyToOne
-	@MapsId("userId")
-	@JoinColumn(name="user_id")
-	private Users user;
-	
-	@ManyToOne
-	@MapsId("postId")
-	@JoinColumn(name="post_id")
-	private Posts post;
-
-	public Likes() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-
-	public Likes(LikesKey id, Users user, Posts post) {
-		super();
-		this.id = id;
-		this.user = user;
-		this.post = post;
-	}
-
-	public LikesKey getId() {
-		return id;
-	}
-
-	public void setId(LikesKey id) {
-		this.id = id;
-	}
-
-	public Users getUser() {
-		return user;
-	}
-
-	public void setUser(Users user) {
-		this.user = user;
-	}
-
-	public Posts getPost() {
-		return post;
-	}
-
-	public void setPost(Posts post) {
-		this.post = post;
-	}
-	
-	
-
-}
+//package com.project.model;
+//
+//import javax.persistence.EmbeddedId;
+//import javax.persistence.Entity;
+//import javax.persistence.JoinColumn;
+//import javax.persistence.ManyToOne;
+//import javax.persistence.MapsId;
+//import javax.persistence.Table;
+//
+//@Entity
+//@Table(name="LikeY")
+//public class Likes {
+//	
+//	@EmbeddedId
+//	LikesKey id;
+//	
+//	@ManyToOne
+//	@MapsId("userId")
+//	@JoinColumn(name="user_id")
+//	private Users user;
+//	
+//	@ManyToOne
+//	@MapsId("postId")
+//	@JoinColumn(name="post_id")
+//	private Posts post;
+//
+//	public Likes() {
+//		super();
+//		// TODO Auto-generated constructor stub
+//	}
+//
+//	public Likes(LikesKey id, Users user, Posts post) {
+//		super();
+//		this.id = id;
+//		this.user = user;
+//		this.post = post;
+//	}
+//
+//	public LikesKey getId() {
+//		return id;
+//	}
+//
+//	public void setId(LikesKey id) {
+//		this.id = id;
+//	}
+//
+//	public Users getUser() {
+//		return user;
+//	}
+//
+//	public void setUser(Users user) {
+//		this.user = user;
+//	}
+//
+//	public Posts getPost() {
+//		return post;
+//	}
+//
+//	public void setPost(Posts post) {
+//		this.post = post;
+//	}
+//	
+//	
+//
+//}
--- src/main/java/com/project/repo/UserDao.java
+++ src/main/java/com/project/repo/UserDao.java
@@ -2,28 +2,50 @@ package com.project.repo;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.project.model.Users;
 import com.project.util.HibernateUtil;
 
 @Repository
+@Transactional
 public class UserDao {
+	
+	private SessionFactory sessfact;
+	
+	public UserDao() {	}
+	
+	@Autowired
+	public UserDao(SessionFactory sessfact) {
+		super();
+		this.sessfact = sessfact;
+	}
 
+//	public List<Users> findAll() {
+//		List<Users> uList = HibernateUtil.getSessionFactory().openSession().createNativeQuery("select * from users", Users.class).list();
+//		return uList;
+//	}
+	
 	public List<Users> findAll() {
-		List<Users> uList = HibernateUtil.getSessionFactory().openSession().createNativeQuery("select * from users", Users.class).list();
-		return uList;
+		return sessfact.openSession().createQuery("from Users", Users.class).list();
 	}
 	
-	public Users findById(Integer i) {
-		Session sess = HibernateUtil.getSessionFactory().openSession();
-//		Transaction tx = sess.beginTransaction();
-		Users u = sess.get(Users.class, i);
-		return u;
+	public Users findById(int id) {
+		return sessfact.openSession().get(Users.class, id);
 	}
+	
+//	public Users findById(Integer i) {
+//		Session sess = HibernateUtil.getSessionFactory().openSession();
+////		Transaction tx = sess.beginTransaction();
+//		Users u = sess.get(Users.class, i);
+//		return u;
+//	}
 
 	public Users update(Users t) {
 		SessionFactory sesfact = HibernateUtil.getSessionFactory();
@@ -34,17 +56,26 @@ public class UserDao {
 		return t;
 	}
 
-	public Users save(Users t) {
-//		//create blank profile before you add users
-//		Profiles blank = new Profiles("no-pic", "I'm boring", "No bio", "No interests");
-//		pDAO.save(blank);
-		Session sess = HibernateUtil.getSessionFactory().openSession();
+//	public Users save(Users t) {
+////		//create blank profile before you add users
+////		Profiles blank = new Profiles("no-pic", "I'm boring", "No bio", "No interests");
+////		pDAO.save(blank);
+//		Session sess = HibernateUtil.getSessionFactory().openSession();
+//		Transaction tx = sess.beginTransaction();
+//		sess.persist(t);
+//		tx.commit();
+//		return t;
+//	}
+	
+	public void save(Users c) {
+//		sessfact.getCurrentSession().save(c);
+		Session sess = sessfact.openSession();
 		Transaction tx = sess.beginTransaction();
-		sess.persist(t);
+		sess.save(c);
 		tx.commit();
-		return t;
-		
 	}
+	
+	
 
 	public Users delete(Integer i) {
 		//Session ses = HibernateUtil.getSessionFactory().openSession();
