5e9652f237bd270aca3e85eacf0fd0fe4cbf198c
CarloAnselmo <CarloAnselmo@gmail.com>
2020-11-11 22:53:36 -0500

    small stuff

--- src/main/java/com/project/controller/UserController.java
+++ src/main/java/com/project/controller/UserController.java
@@ -18,12 +18,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.project.model.Users;
+import com.project.service.EmailService;
 import com.project.service.S3Service;
 import com.project.service.UserService;
 
-
 @Controller
-@CrossOrigin(origins="http://localhost:3000") // Injects the header, allows requests from this origin. Can also use wildcards
+@CrossOrigin(origins = "http://localhost:3000") // Injects the header, allows requests from this origin. Can also use
+												// wildcards
 @RequestMapping("/users")
 @MultipartConfig
 public class UserController {
@@ -68,12 +69,11 @@ public class UserController {
 	public @ResponseBody Users validateUser(@RequestBody Map<String, String> json) {
 		return us.validateLogin(json.get("username"), json.get("password"));
 	}
-	
+
 	@PostMapping("/create")
-	public @ResponseBody Users createUser(@RequestBody Map<String, String> json) 
-	{
-		return us.createUser(json.get("username"), json.get("password"), json.get("firstname"), 
-				json.get("lastname"), json.get("email"));
+	public @ResponseBody Users createUser(@RequestBody Map<String, String> json) {
+		return us.createUser(json.get("username"), json.get("password"), json.get("firstname"), json.get("lastname"),
+				json.get("email"));
 	}
 
 	@PostMapping("/status")
@@ -84,22 +84,23 @@ public class UserController {
 		return us.updateUserStatus(userId, status);
 	}
 
-	@PostMapping(value="/updateBasic")
+	@PostMapping(value = "/updateBasic")
 	public @ResponseBody Users updateBasicInfo(@RequestParam("userId") int userId,
-			@RequestParam(name="image", required=false) MultipartFile image, @RequestParam("username") String username,
-			@RequestParam("firstName") String firstName, @RequestParam("lastName") String lastName,
-			@RequestParam("bio") String bio, @RequestParam("interests") String interests) {
-		
+			@RequestParam(name = "image", required = false) MultipartFile image,
+			@RequestParam("username") String username, @RequestParam("firstName") String firstName,
+			@RequestParam("lastName") String lastName, @RequestParam("bio") String bio,
+			@RequestParam("interests") String interests) {
+
 		String pic = "";
 		Users temp = new Users();
-		
-		if(image==null) {
+
+		if (image == null) {
 			// Do nothing
 		} else if (image.getContentType().contains("image")) {
 			pic = s3s.UploadAvatar(userId, image);
 			temp.setPicUrl(pic);
 		}
-		
+
 		temp.setId((userId));
 		temp.setUsername(username);
 		temp.setFirstname(firstName);
@@ -114,27 +115,19 @@ public class UserController {
 		return removeRecursion;
 	}
 
-	@PostMapping("/updatePic")
-	public @ResponseBody Users updateProfilePic(@RequestParam("userId") int userId,
-			@RequestParam("image") MultipartFile image) {
-		String pic = "";
-		if (image.getContentType().contains("image")) {
-			pic = s3s.UploadAvatar(userId, image);
-		}
-
-		Users removeRecursion = us.updateProfilePic(userId, pic);
-		removeRecursion.setPosts(null);
-		removeRecursion.setLikedPosts(null);
-		return removeRecursion;
-	}
-
 	// This one will probably need to be changed when email feature is added
 	@PostMapping("/updateEmail")
 	public @ResponseBody Users updateEmail(@RequestBody Map<String, String> json) {
 		int userId = Integer.parseInt(json.get("userId"));
 		String email = json.get("email");
+		String oldEmail = json.get("oldEmail");
 
-		// LOGIC TO SEND EMAIL GOES HERE
+		EmailService.sendMail(oldEmail, "Your MochiCircle email has been updated",
+				"<p>Just letting you know that your MochiCircle email has been changed to " + email
+						+ "!</p><br/><p>If you did not request this change, that's too bad!</p>");
+		EmailService.sendMail(email, "Your new MochiCircle email", "<p>Hey!\nYour account that was previously tied to "
+				+ oldEmail
+				+ " has been moved to this email address!</p><br/><p>If you did not make this request, that's too bad!</p>");
 
 		Users removeRecursion = us.updateEmail(userId, email);
 		removeRecursion.setPosts(null);
@@ -147,8 +140,12 @@ public class UserController {
 	public @ResponseBody Users updatePassword(@RequestBody Map<String, String> json) {
 		int userId = Integer.parseInt(json.get("userId"));
 		String password = json.get("password");
+		String email = json.get("email");
+		String firstLetter = password.substring(0, 1);
 
-		// LOGIC TO SEND EMAIL GOES HERE
+		EmailService.sendMail(email, "Your MochiCircle password has changed",
+				"<p>Just letting you know that your MochiCircle password has been changed to something beginning with the character '"
+						+ firstLetter + "'!</p><br/><p>If you did not request this change, good luck!</p>");
 
 		Users removeRecursion = us.updatePassword(userId, password);
 		removeRecursion.setPosts(null);
